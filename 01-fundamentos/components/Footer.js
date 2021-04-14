bc62408cb9c3fbf66e7bea1f872095667a143c93
Jhon <jhon.contreras13@gmail.com>
2021-04-14 18:30:07 -0400

    Componente footer

---
+++ 01-fundamentos/components/Footer.js
@@ -0,0 +1,9 @@
+
+app.component('footer-banco', {
+    // alt+96 template strings 
+    template: /*html*/`
+        <div class="bg-dark text-center my-3 mt-5 text-white">
+            <h3>Footer</h3>
+        </div>
+    `
+})
