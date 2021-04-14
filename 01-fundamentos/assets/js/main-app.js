01a9a5b1e3c95e6acc4c3801df0d3f565d6c8f55
Jhon <jhon@beplan.cl>
2021-04-13 20:42:24 -0400

    v-if v-else

--- 01-fundamentos/assets/js/main-app.js
+++ 01-fundamentos/assets/js/main-app.js
@@ -3,8 +3,9 @@ const app = Vue.createApp({
     data(){
         return{
             titulo: 'Banco con Vue.js',
-            cantidad: 1000,
-            enlace: 'https://youtube.com/'
+            cantidad: 0,
+            enlace: 'https://youtube.com/',
+            estado: false
         }
     }
 
