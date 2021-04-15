edd1c61232970a8e9e0efd37b7516146ae8a7c96
Jhon <jhon.contreras13@gmail.com>
2021-04-14 20:34:13 -0400

    props dinámicos

--- 01-fundamentos/components/Footer.js
+++ 01-fundamentos/components/Footer.js
@@ -1,9 +1,17 @@
 
 app.component('footer-banco', {
+    // los props sirven para pasar parámetros o información al propio web component 
+    // en este caso, le estamos pasando el valor (cantidad que viene de main-app.js) 
+    props: ['valor', 'fecha'],
     // alt+96 template strings 
     template: /*html*/`
         <div class="bg-dark text-center my-3 mt-5 text-white">
-            <h3>Footer</h3>
+            <h3> {{texto}} - {{valor}}</h3>
         </div>
-    `
+    `,
+    data(){
+        return{
+            texto: 'Footer del sitio web dinámico'
+        }
+    }
 })
