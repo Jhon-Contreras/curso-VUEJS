666b5bbd10662872b6565dc3dde33b3a56cb53f8
Jhon <jhon.contreras13@gmail.com>
2021-04-15 21:06:44 -0400

    primer componente

---
+++ 02-vue-cli/hola-mundo/src/components/Titulo.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div >
+        <h1>{{texto}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Titulo',
+        props:{
+            texto: String
+        }
+    }
+</script>
+
+<style scoped>
+   
+         h1{
+             color: rgb(25, 87, 0);
+        }
+   
+</style>
