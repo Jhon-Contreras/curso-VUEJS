2885cdc096896c581716033950b23c35da7d0e18
Jhon <jhon.contreras13@gmail.com>
2021-04-23 20:37:39 -0400

    Ruta con parametro, configuracion básica

--- 03-vue-router/intro-vue-router/src/router/index.js
+++ 03-vue-router/intro-vue-router/src/router/index.js
@@ -17,6 +17,13 @@ const routes = [
     name: 'Blog',
     component: () => import(/* webpackChunkName: "blog" */ '../views/Blog.vue')
   }
+  ,
+  {
+    // ruta con parametros 
+    path: '/blog/:id',
+    name: 'Articulo',
+    component: () => import(/* webpackChunkName: "blog" */ '../views/Articulo.vue')
+  }
 ]
 
 const router = createRouter({
---
+++ 03-vue-router/intro-vue-router/src/views/Articulo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Titulo texto="ruta con parametros" />
+        <h2>Parámetro: {{ $route.params.id }} </h2>
+    </div>
+</template>
+<script>
+
+import Titulo from '../components/Titulo'
+export default {
+    components:{
+        Titulo
+    }
+}
+</script>
+<style lang="">
+    
+</style>
