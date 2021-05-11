a2d776d66a2d47b25ed520a2f6b2b6c8c6c7c54b
Jhon <jhon.contreras13@gmail.com>
2021-05-10 21:45:17 -0400

    Vista articulo con async await, cargando dinamicamente articulos desde vista blog

--- 03-vue-router/intro-vue-router/src/views/Articulo.vue
+++ 03-vue-router/intro-vue-router/src/views/Articulo.vue
@@ -1,7 +1,21 @@
 <template>
     <div>
-        <Titulo texto="ruta con parametros" />
-        <h2>Parámetro: {{ $route.params.id }} </h2>
+        <Titulo texto="Vista Articulo blog" />
+        <!-- <h2>Parámetro: {{ $route.params.id }} </h2> -->
+         <div class="cont-articulo">
+             <div class="box-titulo">
+                 <div>
+                     <span>ID Artículo: {{articulo.id}}</span>
+                 </div>
+                 <div>
+                    <h3> {{articulo.title}} </h3>
+                 </div>
+             </div>
+           
+            <p>{{ articulo.body }} </p>
+    
+            <a href="../blog" class="volver"> Volver a todos los posts</a>
+         </div>
     </div>
 </template>
 <script>
@@ -10,9 +24,42 @@ import Titulo from '../components/Titulo'
 export default {
     components:{
         Titulo
+    },
+    data() {
+        return {
+            articulo: {
+
+            }
+        }
+    },
+    methods: {
+        async consumirArticulo(){
+            try {
+                const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+                const objeto = await data.json()
+                console.log(objeto);
+                this.articulo = objeto
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+    created(){
+        this.consumirArticulo()
     }
 }
 </script>
-<style lang="">
-    
+<style lang="css">
+     .cont-articulo{
+       width: 100%;
+       max-width: 1200px;
+       margin: 0 auto;
+       height: auto;
+     
+        padding-bottom: 1em;
+        padding-top: 1em;
+   }
+   .volver{
+       color: green;
+   }
 </style>
--- 03-vue-router/intro-vue-router/src/views/Blog.vue
+++ 03-vue-router/intro-vue-router/src/views/Blog.vue
@@ -1,10 +1,24 @@
 <template>
     <div>
-         <Titulo texto="Titulo de mi blog" />
+         <Titulo texto="Blog en VUE" />
          <!-- <button @click="consumirApi"> Consumir API</button> -->
          <div v-for="item in arrayBlog" :key="item.id">
-             {{ item.title }}
+             <!-- se usan comillas invertidas para agregar js en el html -->
+             <!-- <router-link :to="`/blog/${item.id}`"> {{ item.title }}</router-link> -->
+            
          </div>
+          <div class="grid-blog">
+              <div class="item" v-for="item in arrayBlog" :key="item.id">
+                
+                      <div class="post-id">
+                            <span>post id: <br> {{item.id}} </span>
+                      </div>
+                      <div class="post-title">
+                          <h2> <router-link :to="`/blog/${item.id}`"> {{ item.title }}</router-link></h2>
+                      </div>
+              
+              </div>
+          </div>
     </div>
 </template>
 <script>
@@ -36,6 +50,67 @@ export default {
     }
 }
 </script>
-<style lang="">
+<style lang="css">
+   .grid-blog{
+       width: 100%;
+       max-width: 1200px;
+       margin: 0 auto;
+       height: auto;
+       display: grid;
+       grid-template-columns: 1fr 1fr 1fr 1fr;
+       grid-template-rows: auto;
+       gap: 20px 20px;
+       grid-template-areas:
+        ". . . .";
+        padding-bottom: 1em;
+        padding-top: 1em;
+   }
+   @media screen and (max-width: 780px) {
+       .grid-blog{
+           grid-template-columns: 1fr 1fr;
+            grid-template-areas:
+            ". . ";
+       } 
+   }
+   .grid-blog .item{
+       height: auto;
+       padding-top: 5px;
+       padding-bottom: 5px;
+       border: 1px solid rgb(209, 209, 209);
+       text-align: left;
+        display: grid;
+       grid-template-columns: 0.5fr 1.5fr;
+       grid-template-rows: auto;
+       gap: 0px 0px;
+       grid-template-areas:
+        ". .";
+   }
+   .grid-blog .item h2{
+      
+       font-weight: 400;
+       font-size: 14px;
+   }
+    .grid-blog .item h2 a{
+        text-decoration: none;
+        color: black;
+    }.grid-blog .item h2 a:hover{
+        color: green;
+    }
+    .grid-blog .item .post-id{
+        padding-left: 5px;
+        text-align: center;
+        border-right: 1px solid rgb(209, 209, 209);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .grid-blog .item .post-id span{
+        font-weight: 700;
+        font-size: 11px;
+    }
+    .grid-blog .item .post-title{
+        padding-left: 5px;
+        text-align: left;
+    }
     
 </style>
