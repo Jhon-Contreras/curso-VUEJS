f1bae73d8a31b91510ed4d4ebf6bbd366019d37b
Jhon <jhon.contreras13@gmail.com>
2021-05-10 20:58:40 -0400

    Consumir Api con Async y Await

--- 03-vue-router/intro-vue-router/src/views/Blog.vue
+++ 03-vue-router/intro-vue-router/src/views/Blog.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
          <Titulo texto="Titulo de mi blog" />
+
+         <button @click="consumirApi"> Consumir API</button>
+
+         <div v-for="item in arrayBlog" :key="item.id">
+             {{ item.title }}
+         </div>
     </div>
 </template>
 <script>
@@ -8,7 +14,24 @@ import Titulo from '../components/Titulo'
 export default {
     components:{
         Titulo
-    }
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {  
+        async consumirApi(){
+            try{
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+                const array = await data.json()
+                console.log(array);
+                this.arrayBlog = array;
+            } catch (error){
+                console.log(error);
+            }
+        }
+    },
 }
 </script>
 <style lang="">
