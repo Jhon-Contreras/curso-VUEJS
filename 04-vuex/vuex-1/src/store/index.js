948a129922415b9bcfd97d47f3061058bd0e9bfa
Jhon <jhon.contreras13@gmail.com>
2021-05-11 20:52:15 -0400

    state, mutations, actions

--- 04-vuex/vuex-1/src/store/index.js
+++ 04-vuex/vuex-1/src/store/index.js
@@ -1,11 +1,22 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // siempre state, irá en una propiedad computada 
   state: {
+    contador: 100
   },
+  // las mutaciones modifican el state
   mutations: {
+    incrementar(state){
+      state.contador = state.contador + 10
+    }
   },
+  // accionar una mutacion 
   actions: {
+    //commit ejecuta una mutacion
+    accionIncrementar({commit}){
+      commit('incrementar')
+    } 
   },
   modules: {
   }
--- 04-vuex/vuex-1/src/views/Home.vue
+++ 04-vuex/vuex-1/src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+ 
+    <h1 :style="colorContador"> {{titulo}}: {{ contador }} </h1>
+
+    <button @click="accionIncrementar"> Aumentar </button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {mapState, mapMutations, mapActions} from 'vuex'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+   
+  },
+  data() {
+    return {
+      titulo: 'Contador Vuex'
+    }
+  },
+  // mapeamos el state con ... para ser utilizado 
+  computed:{
+    ...mapState(['contador']),
+    colorContador(){
+      return  [this.contador > 100 ? {'color' : 'green'} : {'color' : 'red'}]
+    }
+  },
+  // las mutaciones y las acciones se mapean en los metodos 
+  methods: {
+    ...mapMutations(['incrementar']), //no se recomienda esto
+    ...mapActions(['accionIncrementar'])
+  },
 }
 </script>
