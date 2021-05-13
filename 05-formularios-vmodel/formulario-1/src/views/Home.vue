ba4cb982e1b2f2a12ea7fa6a59279fd326fa411e
Jhon <jhon.contreras13@gmail.com>
2021-05-12 20:42:59 -0400

    Formulario, evento @submit

--- 05-formularios-vmodel/formulario-1/src/views/Home.vue
+++ 05-formularios-vmodel/formulario-1/src/views/Home.vue
@@ -1,7 +1,48 @@
 <template>
-  <div class="home">
-   Inicio
-  </div>
+    <div class="row d-flex justify-content-center mt-4">
+      <div class="col-xl-8">
+        <div class="card py-5 px-5">
+          <!-- @submit recibe un metodo  -->
+          <!-- @submit.prevent se usa para evitar el procesamiento por get por defecto de html  -->
+          <form @submit.prevent="procesarFormulario">
+            <!-- v-model.trim elimina espacios al enviar un form  -->
+              <input type="text" class="form-control my-2" placeholder="Ingrese nombre" v-model.trim="tarea.nombre">
+             
+              <!-- checkbox's  -->
+              <div class="form-check form-check-inline">
+                  <input type="checkbox" class="form-check-input" id="check-1" v-model="tarea.categorias" value="javascript">
+                  <label for="check-1" class="form-check-label">Javascript</label>
+              </div>
+              <div class="form-check form-check-inline">
+                  <input type="checkbox" class="form-check-input" id="check-2" v-model="tarea.categorias" value="node js">
+                  <label for="check-2" class="form-check-label">Node JS</label>
+              </div>
+              <!-- radio buttons  -->
+              <div class="mt-2">
+                <div class="form-check form-check-inline">
+                  <input type="radio" class="form-check-input" id="radio-1" v-model="tarea.estado" value="urgente">
+                  <label for="radio-1" class="form-check-label">Urgente</label>
+                </div>
+
+                 <div class="form-check form-check-inline">
+                  <input type="radio" class="form-check-input" id="radio-2" v-model="tarea.estado" value="relax">
+                  <label for="radio-2" class="form-check-label">Relax</label>
+                </div>
+              </div>
+              <!-- numeros  -->
+              <div class="mt-2">
+                <!-- v-model.number transforma un string en un numero  -->
+                  <input type="number" class="form-control" v-model.number="tarea.numero">
+              </div>
+
+              <button class="btn btn-success mt-3" type="submit">Procesar</button>
+              <hr>
+          </form>
+
+          <p>{{tarea}}</p>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -10,8 +51,24 @@
 
 export default {
   name: 'Home',
-  components: {
-  
+    components: {
+    
+    },
+    data() {
+      return {
+        tarea:{
+          nombre: '',
+          categorias: [],
+          estado: '',
+          numero: 0
+        }
+      }
+    },
+    methods: {
+      procesarFormulario(){
+        console.log(this.tarea);
+      }
+    },
   }
-}
+ 
 </script>
